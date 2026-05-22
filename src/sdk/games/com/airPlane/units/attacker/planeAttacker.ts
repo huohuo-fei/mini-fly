e3cf3cac11f4b1af2ee7ecbca8cc6f37d7428b85
huofei <1503066283@qq.com>
2026-05-22 20:54:17 +0800

    新增护盾效果

--- src/sdk/games/com/airPlane/units/attacker/planeAttacker.ts
+++ src/sdk/games/com/airPlane/units/attacker/planeAttacker.ts
@@ -14,12 +14,17 @@ export class PlaneAttacker implements IMiniGam {
   attackerX: number = 0;
   attackerY: number = 0;
   gameParams: IMiniGameParams;
+  cx: number = 0;
+  cy: number = 0;
 
   // 子弹
   bullets: PlaneBullet[] = [];
   bulletType = MyBulletType.NORMAL;
   bulletTimer: number | null = null;
   size: number = 1;
+
+  // 离屏canvas todo:后续由外部统一管理
+  offScreenCanvas: HTMLCanvasElement | null = null;
 
   constructor(params: IMiniGameParams) {
     const playerX = params.canvasWidth / 2 - 15;
@@ -54,13 +59,13 @@ export class PlaneAttacker implements IMiniGam {
           config.x = cx - config.w;
           config.y = cy;
           const bullet = new PlaneBullet(this.bulletType, this, config);
-          this.bullets.push(bullet);  
+          this.bullets.push(bullet);
           config.x = cx + config.w;
           config.y = cy;
           const bullet2 = new PlaneBullet(this.bulletType, this, config);
           this.bullets.push(bullet2);
-        }else if(this.size === 3){
-          config.x = cx - config.w *2;
+        } else if (this.size === 3) {
+          config.x = cx - config.w * 2;
           config.y = cy;
           const bullet = new PlaneBullet(this.bulletType, this, config);
           this.bullets.push(bullet);
@@ -71,7 +76,7 @@ export class PlaneAttacker implements IMiniGam {
           config.x = cx;
           config.y = cy;
           config.speedY = speedY * 1.05;
-          
+
           const bullet3 = new PlaneBullet(this.bulletType, this, config);
           this.bullets.push(bullet3);
         }
@@ -90,6 +95,8 @@ export class PlaneAttacker implements IMiniGam {
   updatePos(x: number, y: number) {
     this.attackerX = x;
     this.attackerY = y;
+    this.cx = x + this.PLAYER_WIDTH / 2;
+    this.cy = y + this.PLAYER_HEIGHT / 2;
   }
 
   updatePosX(x: number) {
@@ -108,6 +115,7 @@ export class PlaneAttacker implements IMiniGam {
       gameParams.canvasWidth - PLAYER_WIDTH - 5
     );
     this.attackerX = resX;
+    this.cx = this.attackerX + this.PLAYER_WIDTH / 2;
   }
 
   drawPlayer(ctx: CanvasRenderingContext2D) {
@@ -132,12 +140,70 @@ export class PlaneAttacker implements IMiniGam {
 
   render(ctx: CanvasRenderingContext2D) {
     // 绘制
+    this.drawLight(ctx);
     this.drawPlayer(ctx);
+
 
     for (let i = 0; i < this.bullets.length; i++) {
       const b = this.bullets[i];
       b.render(ctx);
     }
+  }
+
+  // 绘制光罩
+  drawLight(ctx: CanvasRenderingContext2D) {
+    const { PLAYER_WIDTH, cx, cy } = this;
+    if(!this.offScreenCanvas){
+      this.offScreenCanvas = document.createElement('canvas');
+    }
+    this.offScreenCanvas.width = PLAYER_WIDTH*2
+    this.offScreenCanvas.height = PLAYER_WIDTH
+    const offScreenCtx = this.offScreenCanvas.getContext('2d');
+    if(!offScreenCtx)return
+    offScreenCtx.clearRect(0, 0, PLAYER_WIDTH*2, PLAYER_WIDTH);
+
+
+    // 做一个线性渐变，从内到外，颜色从透明到不透明
+    const gradient = ctx.createLinearGradient(
+      0,
+      0,
+      PLAYER_WIDTH * 2,
+      0
+    );
+    gradient.addColorStop(0, 'rgba(0, 0, 0, 0)');
+    gradient.addColorStop(0.5, 'rgba(0, 0, 0, 1)');
+    gradient.addColorStop(1, 'rgba(0, 0, 0, 0)');
+
+    // 绘制护盾主体
+    offScreenCtx.beginPath();
+    offScreenCtx.fillStyle = gradient;
+    offScreenCtx.fillRect(
+      0,
+      0,
+      PLAYER_WIDTH * 2,
+      PLAYER_WIDTH
+    );
+    offScreenCtx.fill();
+
+    offScreenCtx.globalCompositeOperation = "source-in";
+    offScreenCtx.beginPath();
+    offScreenCtx.moveTo(PLAYER_WIDTH, PLAYER_WIDTH);
+    offScreenCtx.arc(PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_WIDTH, Math.PI, 2 * Math.PI);
+    offScreenCtx.closePath();
+    const outerGlowGrad = offScreenCtx.createRadialGradient(
+      PLAYER_WIDTH,
+      PLAYER_WIDTH,
+      PLAYER_WIDTH - 10,
+      PLAYER_WIDTH,
+      PLAYER_WIDTH,
+      PLAYER_WIDTH
+    );
+    outerGlowGrad.addColorStop(0, 'rgba(0, 180, 240, 0)');
+    outerGlowGrad.addColorStop(0.8, 'rgba(0, 200, 255, 0.35)');
+    outerGlowGrad.addColorStop(1, 'rgba(0, 210, 255, 0.92)');
+    offScreenCtx.fillStyle = outerGlowGrad;
+    offScreenCtx.fill();
+    ctx.drawImage(this.offScreenCanvas, cx - PLAYER_WIDTH , cy - PLAYER_WIDTH,);
   }
   actionStart = () => {};
   actionEnd = () => {};
